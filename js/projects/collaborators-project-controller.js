7506a44519b43aee86e48a1f1d0ca84c5b6d9e23
petyopetkov <petyo.petkov@mail.bg>
2016-03-23 10:11:51 +0200

    Display project collaborators email implemented

--- js/projects/collaborators-project-controller.js
+++ js/projects/collaborators-project-controller.js
@@ -8,6 +8,10 @@
             .then(function (allCollaborators) {
                 vm.collaborators = allCollaborators;
             });
+        projects.getById($routeParams.id)
+            .then(function (foundProject) {
+                vm.project = foundProject;
+            });
     }
     angular.module('myApp.controllers')
         .controller('CollaboratorsProjectController', ['$routeParams', 'projects', 'identity', CollaboratorsProjectController]);
